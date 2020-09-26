c26bec3a76bb700d0cf251483c1e0e0f5e539e56
Shreyam Adhikari <shreyam1008@gmail.com>
2020-09-26 19:25:02 +0545

    coket initiated

--- server/index.js
+++ server/index.js
@@ -4,10 +4,25 @@ const http = require('http')
 
 const PORT = process.env.PORT || 5000
 
+const router = require("./router")
+
 const app = express()
 const server = http.createServer(app)
 const io = socketio(server)
 
+//socket.io code
+
+io.on('connection', (socket)=>{
+    console.log("we have a connection to socket")
+
+    socket.on('disconnect', =>{
+        console.log('user left')
+    })
+
+})
+
+app.use(router)
+    
 server.listen(PORT, ()=>console.log('server started yeppie '))
 
 
