3b43813045ab9051aed3fa2c1efa49f279a43d1f
Shreyam Adhikari <shreyam1008@gmail.com>
2020-10-06 14:44:24 +0545

    MODELS WORKING

--- server/index.js
+++ server/index.js
@@ -1,28 +1,28 @@
-const express = require('express')
-const socketio = require('socket.io')
-const http = require('http')
+const express = require("express");
+const socketio = require("socket.io");
+const http = require("http");
 
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 5000;
 
-const router = require("./router")
+const router = require("./router");
 
-const app = express()
-const server = http.createServer(app)
-const io = socketio(server)
+const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
 
 //socket.io code
 
-io.on('connection', (socket)=>{
-    console.log("we have a connection to socket")
+io.on("connection", (socket) => {
+  console.log("we have a connection to socket");
+  socket.in("join", ({ name, room }) => {
+    console.log(name, room);
+  });
 
-    socket.on('disconnect', ()=>{
-        console.log('user left')
-    })
+  socket.on("disconnect", () => {
+    console.log("user left");
+  });
+});
 
-})
+app.use(router);
 
-app.use(router)
-    
-server.listen(PORT, ()=>console.log('server started yeppie '))
-
-
+server.listen(PORT, () => console.log("server started yeppie "));
