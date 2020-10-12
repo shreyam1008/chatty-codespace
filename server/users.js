a36446ac54c934031c62ef10d3edc3db2d318b71
Shreyam Adhikari <shreyam1008@gmail.com>
2020-10-12 19:26:06 +0545

    getting all users in rom

--- server/users.js
+++ server/users.js
@@ -25,11 +25,13 @@ const removeUser = (id) => {
     }
 }
 
-const getUser = () => {
-    
+const getUser = (id) => {
+    users.find((user) => user.id === id)
 }
 
-const getUserInRoom = () => {
-    
+const getUsersInRoom = (room) => {
+    users.filter((user) => user.room===room)
 }
+
+module.exports = {addUser. removeUser, getUser, getUsersInRoom}
 
